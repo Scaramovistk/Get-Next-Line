35b8e6a914dfbef0955ea036f396c9d80d327f8c
Scaramovistk <85752923+Scaramovistk@users.noreply.github.com>
2022-02-03 17:01:56 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -10,197 +10,99 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "get_next_line.h"
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "get_next_line.h"
 #include <stdio.h>
 
-
-size_t	ft_strlen(const char *str)
+char	*ft_fdline(char *rest, int fd)
 {
-	size_t		size;
+	char	buf[BUFFER_SIZE];
+	ssize_t	r_bytes;
 
-	size = 0;
-	while (str[size] != '\0')
-		size++;
-	return (size);
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	lendst;
-	size_t	lensrc;
-	size_t	rest;
-
-	i = 0;
-	rest = size;
-	while (dst[i] && rest > 0)
+	r_bytes = 1;
+	while (!ft_strchr(rest, '\n') && r_bytes > 0)
 	{
-		rest--;
-		i++;
-	}
-	lendst = ft_strlen(dst);
-	lensrc = ft_strlen(src);
-	if (rest == 0)
-		return (lensrc + size);
-	i = 0;
-	while (rest > 1 && src[i] != '\0')
-	{
-		dst[(lendst + i)] = src[i];
-		rest--;
-		i++;
-	}
-	dst[(lendst + i)] = '\0';
-	return (lendst + lensrc);
-}
-
-char	*ft_strnjoin(char const *s1, char const *s2, int i)
-{
-	size_t	len;
-	size_t	lens1;
-	int		j;
-	char	*nstr;
-
-	j = 0;
-	if (!s1 || !s2)
-		return (NULL);
-	lens1 = ft_strlen(s1);
-	len = lens1 + i + 1;
-	nstr = malloc(sizeof(char) * len);
-	if (!nstr)
-		return (NULL);
-	nstr[0] = '\0';
-	ft_strlcat(nstr, s1, len);
-	while (j != i)
-	{
-		nstr[lens1] = s2[j];
-		lens1++;
-		j++;
-	}
-	free((void *)s1);
-	return (nstr);
-}
-
-static char	*ft_strndup(char const *src, size_t size)
-{
-	size_t	i;
-	char	*dest;
-
-	i = 0;
-	dest = malloc(sizeof(char) * size + 1);
-	if (dest == NULL)
-		return (NULL);
-	while (src[i] && i < size)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-int	ft_strchr(const char *s, int c)
-{
-	int				i;
-	unsigned char	*str;
-	unsigned char	uc;
-
-	str = (unsigned char *)s;
-	uc = (unsigned char)c;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == uc)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-//////////////////////////////
-
-char	*ft_fdline(int fd)
-{
-	char	buf[BUFFER_SIZE + 1];
-	char	*dst;
-	char	*rest;
-	int		size;
-	int		i;
-	int		j;
-
-	size = 0;
-	i = 0;
-	rest = (char *)'\0';
-	while (1)
-	{
-		read(fd, buf, BUFFER_SIZE);
-		rest = ft_strnjoin(rest, buf, BUFFER_SIZE);
-		j = 0;
-		while (j != BUFFER_SIZE)
+		r_bytes = read(fd, buf, BUFFER_SIZE);
+		if (r_bytes <= 0)
+			return (0);
+		if (!rest)
 		{
-			if (buf[j + i] == '\0')
-			{
-				while (buf[size])
-					size++;
-				dst = ft_strndup(buf, size);
-				return (dst);
-			}
-			j++;
+			rest = malloc(sizeof(char) * 1);
+			if (!rest)
+				return (0);
+			rest[0] = '\0';
 		}
-		i++;
+		rest = ft_strnjoin(rest, buf, r_bytes);
 	}
-	// while (!ft_strchr(buf, '\0') && BUFFER_SIZE != 0)
-	// {
-	// 	read(fd, buf, BUFFER_SIZE);
-	// 	rest = ft_strnjoin(rest, buf, BUFFER_SIZE);
-	// }
-	return (0);
+	return (rest);
+}
+
+char	*ft_fdover(char *rest, int size)
+{
+	int		end;
+	char	*tmp;
+
+	end = 0;
+	while (rest[end + size])
+		end++;
+	tmp = ft_strndup(rest + size, end);
+	if (!tmp)
+		return (0);
+	free(rest);
+	rest = ft_strndup(tmp, end);
+	if (!rest)
+		return (0);
+	free(tmp);
+	return (rest);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*buf;
+	static char	*rest;
 	char		*dst;
 	int			size;
-	static int	bg;
-	int			end;
 
-	end = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
 	size = 0;
-	if (!bg)
-		bg = 0;
-	buf = ft_fdline(fd);
-	while (buf[size + bg] != '\n' && buf[size + bg])
+	rest = ft_fdline(rest, fd);
+	if (0 == rest)
+		return (0);
+	while (rest[size] && rest[size] != '\n')
 		size++;
-	dst = ft_strndup(buf + bg, size); //Need protection for malloc
-	bg += size;
+	if (size == 0)
+	{
+		if (1)
+			free(rest);
+		return (0);
+	}
+	dst = ft_strndup(rest, size + 1);
+	if (!dst)
+		return (0);
+	rest = ft_fdover(rest, size);
 	return (dst);
 }
 
-#include <stdio.h>
-#include <fcntl.h>
-int	main(void)
-{
-	char	*ret;
-	int		fd;
-	size_t	i;
+// #include <stdio.h>
+// #include <fcntl.h>
+// int	main(void)
+// {
+// 	char	*ret;
+// 	int		fd;
+// 	size_t	i;
 
-	fd = open("test.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		printf("OPEN ERROR\n");
-		return (0);
-	}
-	i = 0;
-	while (i < 2)
-	{
-		ret = get_next_line(fd);
-		printf("Str: %s\n", ret);
-		free(ret);
-		++i;
-	}
-	return (0);
-}
-
+// 	fd = open("test.txt", O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		printf("OPEN ERROR\n");
+// 		return (0);
+// 	}
+// 	i = 0;
+// 	while (i < 2)
+// 	{
+// 		ret = get_next_line(fd);
+// 		printf("Str: %s\n", ret);
+// 		free(ret);
+// 		++i;
+// 	}
+// 	return (0);
+// }
