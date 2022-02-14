a91dcc8f944d79c601598bdded545a37ac308dcc
Scaramovistk <85752923+Scaramovistk@users.noreply.github.com>
2022-02-14 16:26:21 +0100

    Update get_next_line.c

--- With Main and Test Files/get_next_line.c
+++ With Main and Test Files/get_next_line.c
@@ -6,35 +6,44 @@
 /*   By: gscarama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 20:13:42 by gscarama          #+#    #+#             */
-/*   Updated: 2022/02/09 08:29:45 by gscarama         ###   ########.fr       */
+/*   Updated: 2022/02/14 08:29:45 by gscarama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
+
+char	*ft_free(char *str)
+{
+	free(str);
+	return (0);
+}
 
 char	*ft_fdline(char *rest, int fd)
 {
-	char	buf[BUFFER_SIZE];
+	char	*buf;
 	ssize_t	r_bytes;
 
+	buf = malloc(sizeof(buf) * BUFFER_SIZE + 1);
+	if (!buf)
+		return (0);
 	r_bytes = 1;
 	while (!ft_strchr(rest, '\n') && r_bytes > 0)
 	{
 		r_bytes = read(fd, buf, BUFFER_SIZE);
 		if (r_bytes < 0)
-			return (0);
-		if (!rest && r_bytes > 0)
+			return (ft_free(buf));
+		if (!rest)
 		{
 			rest = malloc(sizeof(char) * 1);
 			if (!rest)
-				return (0);
+				return (ft_free(buf));
 			rest[0] = '\0';
 		}
 		rest = ft_strnjoin(rest, buf, r_bytes);
 		if (!rest)
-			return (0);
+			return (ft_free(buf));
 	}
+	free(buf);
 	return (rest);
 }
 
@@ -45,14 +54,11 @@ char	*ft_fdover(char *rest, ssize_t size)
 
 	end = 0;
 	if (!rest[size])
-	{
-		free(rest);
-		return (0);
-	}
+		return (ft_free(rest));
 	end = ft_strlen(&rest[size + 1]);
 	tmp = malloc(sizeof(char) * (end + 1));
 	if (!tmp)
-		return (0);
+		return (ft_free(rest));
 	end = 0;
 	while (rest[size + end + 1])
 	{
@@ -66,9 +72,9 @@ char	*ft_fdover(char *rest, ssize_t size)
 
 char	*get_next_line(int fd)
 {
-	static char		*rest;
-	char			*dst;
-	ssize_t			size;
+	static char	*rest;
+	char		*dst;
+	ssize_t		size;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
@@ -79,8 +85,6 @@ char	*get_next_line(int fd)
 	while (rest[size] && rest[size] != '\n')
 		size++;
 	dst = ft_strndup(rest, size + 1);
-	if (!dst)
-		return (0);
 	rest = ft_fdover(rest, size);
 	return (dst);
 }
